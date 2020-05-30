0048f93041bdcae3289fa5565378f1ee4d8cb02f
mainak1 <mainak@trevious.com>
2020-05-30 23:45:15 +0530

    ffhjh

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -4,20 +4,20 @@ import { AccountsService } from './accounts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/database/entity/user.entity';
 import { Income } from 'src/database/entity/income.entity';
-import { AwsModule } from 'src/aws/aws.module';
+// import { AwsModule } from 'src/aws/aws.module';
 import { EPin } from 'src/database/entity/epin.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Income, EPin]),
-    AwsModule.forRootAsync({
-      useFactory: async () => ({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION,
-      }),
-      inject: [],
-    }),
+    // AwsModule.forRootAsync({
+    //   useFactory: async () => ({
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    //     region: process.env.AWS_REGION,
+    //   }),
+    //   inject: [],
+    // }),
   ],
   controllers: [AccountsController],
   providers: [AccountsService]
