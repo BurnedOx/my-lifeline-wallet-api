b808617a6d5c26f4a2f9677d5037a58fca36ebce
Mainak Debnath <mainak@triveous.com>
2020-07-25 12:58:01 +0530

    updated withdrawaldto

--- src/withdrawal/withdrawal.dto.ts
+++ src/withdrawal/withdrawal.dto.ts
@@ -2,8 +2,8 @@ import { IsNumber, Min, Max, IsDivisibleBy } from "class-validator";
 
 export class WithdrawalDTO {
     @IsNumber()
-    @Min(300)
-    @Max(2000)
+    @Min(200)
+    @Max(3000)
     @IsDivisibleBy(100)
     withdrawAmount: number;
 }
